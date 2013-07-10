62fb2ea3ed1b99ab38184298ad3127eddd14a3f0
tchaikovsky <holiday@princeton.edu>
2013-07-10 19:17:45 -0400

    removed codec specifying flags, as these cause errors on work computer

--- plotOutput.py
+++ plotOutput.py
@@ -1,7 +1,8 @@
 import numpy as np
 import os
-#import matplotlib
-#matplotlib.use("Agg")
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 
 def genData(fileName):
     pathToFile = os.path.realpath(fileName)
@@ -11,7 +12,7 @@ def genData(fileName):
     f.close()
     begin = 0
     comma = 1
-#create dict from header, based on key=value format in csv
+    #create dict from header, based on key=value format in csv
     while comma > 0:
         comma = paramstr.find(",")
         equals = paramstr.find("=")
@@ -25,25 +26,30 @@ def genData(fileName):
     data = np.genfromtxt(pathToFile, delimiter=",", skip_header=1)
     return params, data
 
-if __name__=="__main__":
-    import sys
-    import matplotlib.pyplot as plt
-    #import matplotlib.animation as animation
-    params, data = genData(sys.argv[1])
+def animateTimeCourse(data, params, fileName, cmap='Paired', fps=10, bitrate=14400, containerType='.mkv'):
+    import matplotlib.animation as animation
     fig = plt.figure()
+    plt.text(params['n']/2,1.05*params['n'],'Evolution of adjacency matrix in preferential attachment model', ha='center', fontsize=16)
     ims = []
-    f = open('pngList.txt', "w")
     for i in range(params['nSteps']/params['dataInterval']):
-        fig = plt.figure(i)
-        plt.pcolormesh(data[(i)*params['n']:(i+1)*params['n'],1:params['n']+1], cmap='Paired')
-        plt.savefig("fig" + str(i) + ".png", format="png")
-        f.write("fig" + str(i) + ".png\n")
-    f.close()
-#        ims.append((plt.pcolormesh(data[(i)*n:(i+1)*n,1:n+1], cmap='Greys'),))
-#    tcAnim = animation.ArtistAnimation(fig, ims, interval=10)
-#    plt.show()    
-     #metadata = dict(artist='alexander holiday')
-    #writer = FFMpegWriter(fps=5, metadata=metadata)
-#    Writer = animation.MencoderWriter(fps=5, codec='ffmpeg', bitrate=1800, metadata={'artist':'alexander holiday'})
-#    tcAnim.save('test.mp4', writer=writer)
+        ims.append((plt.pcolormesh(data[(i)*params['n']:(i+1)*params['n'],1:params['n']+1], cmap=cmap),))
+    tcAnim = animation.ArtistAnimation(fig, ims, interval=10)
+    metadata = dict(artist='alexander holiday')
+    writer = animation.FFMpegWriter(fps=fps, bitrate=bitrate, metadata=metadata)
+    us = fileName.find('_')
+    animFileName = "paAnim" + fileName[us:-4] + containerType
+    tcAnim.save(animFileName, writer=writer)
 
+if __name__=="__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputFiles', nargs='+')
+    parser.add_argument('-c', '--cmap', nargs=1, default='Paired', choices=[m for m in plt.cm.datad])
+    parser.add_argument('--fps', nargs='1', type=int, default=10)
+    parser.add_argument('-br', '--bitrate', nargs=1, type=int, default=14400)
+    parser.add_argument('-ct', '--container-type', nargs=1, default='.mkv')
+    args = parser.parse_args()
+    print args
+    for csv in args.inputFiles:
+        params, data = genData(csv)
+        animateTimeCourse(data, params, csv, args.cmap, args.fps, args.bitrate, args.container_type)
