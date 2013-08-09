b2127a95939061a43f65a9e867b6f6a9c3afb560
Alexander Holiday <holiday@princeton.edu>
2013-08-09 18:04:47 -0400

    using agg backend for cluster submissions

--- plotOutput.py
+++ plotOutput.py
@@ -2,6 +2,8 @@ from multiprocessing import Pool
 import getGraphProps as gGP
 import numpy as np
 import os
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 def genData(fileName):
