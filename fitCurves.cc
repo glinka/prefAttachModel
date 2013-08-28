e9dc2b24a7e56516a84afae1b137cb6eab6df0a8
tchaikovsky <holiday@princeton.edu>
2013-08-28 16:09:03 -0400

    program now requires g++ >=4.5 for lambda function support, now want to concantenate all fns and return, likely run into memory sharing issues

--- fitCurves.cc
+++ fitCurves.cc
@@ -1,3 +1,5 @@
+#include <Eigen/Dense>
+#include <Eigen/LU>
 #include <Eigen/Eigenvalues>
 #include "calcGraphProps.h"
 #include "fitCurve.h"
@@ -5,13 +7,29 @@
 using namespace std;
 using namespace Eigen;
 
-double test(double x) {
-  return x;
-}
-
-fxs fitCurves::fitFx(const MatrixXd &xData,  const MatrixXd &yData, vector<fx> toFit) {
-  fxs f;
-  f.push_back(test);
+fxs fitCurves::fitFx(const dMatrix &xData, const dMatrix &yData, fxs toFit) {
+  //data points are (xData[i], yData[i])
+  //"vectors" are here considered to be columnar
+  int nFns = toFit.size();
+  int nPts = xData.size();
+  int i, j;
+  vector<double> v;
+  MatrixXd fxsEval(nPts, nFns);
+  MatrixXd yDataCpy(nPts, 1);
+  //populate fxsEval with the evaluations of the f(x)s at the given points
+  for(i = 0; i < nPts; i++) {
+    for(j = 0; j < nFns; j++) {
+      fxsEval(i, j) = (*fxs[j])(xData[i]);
+    }
+    yDataCpy(i) = yData[i];
+  }
+  MatrixXd coeffs(nFns, 1);
+  LUDecomposition<MatrixXd> lu((fxsEval.transpose())*fxsEval);
+  coeffs = ((lu.inverse())*(fxsEval.transpose()))*yDataCpy;
+  vector<double> coeffsVect;
+  for(i = 0; i < nFns; i++) {
+    coeffsVect.push_back(coeffs(i));
+  }
   return f;
 } 
 
--- fitCurves.h
+++ fitCurves.h
@@ -1,17 +1,17 @@
 #ifndef FIT_CURVES_H
 #define FIT_CURVES_H
-#include <Eigen/Dense>
 #include <vector>
 
 typedef double (*fx)(double);
-typedef double (*fxy)(double);
+typedef double (*fxy)(double, double);
 typedef std::vector<fx> fxs;
+//typedef std::vector<std::vector<double>> dMatrix;
 
 class fitCurves {
  public:
   fitCurves() {};
   ~fitCurves() {};
-  fxs fitFx(const Eigen::MatrixXd &xData, const Eigen::MatrixXd &yData, fxs toFit);
+  fxs fitFx(const std::vector<double> &xData, const std::vector<double> &yData, fxs toFit);
  private:
 };
   
